af146c8218113ae2bd68794e895d7c2a6aa89423
Lavi Ionas <laviionas772@gmail.com>
2025-03-10 19:07:22 -0400

    set up pydantic types

--- app/main.py
+++ app/main.py
@@ -1,31 +1,15 @@
-from fastapi import FastAPI, Depends
-from sqlalchemy.orm import Session
-from app import models, schemas, database
+from fastapi import FastAPI, HTTPExeption, Depends
+from pydantic import BaseModel
+from typing import List, Annotated
 
 app = FastAPI()
 
-database.Base.metadata.create_all(bind=database.engine)
+class ChoiceBase(BaseModel):
+    choice_text : str
+    is_correct : bool
 
-# Dependency to get the database session
-def get_db():
-    db = database.SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+class QuestionBase(BaseModel):
+    question_text : str
+    choices: List[ChoiceBase]
 
-@app.get("/")
-def read_root():
-    return {"message": "Welcome to the FastAPI AWS-powered API!"}
 
-@app.post("/data/", response_model=schemas.DataResponse)
-def create_data(data: schemas.DataCreate, db: Session = Depends(get_db)):
-    db_data = models.Data(value=data.value)
-    db.add(db_data)
-    db.commit()
-    db.refresh(db_data)
-    return db_data
-
-@app.get("/data/")
-def get_all_data(db: Session = Depends(get_db)):
-    return db.query(models.Data).all()
