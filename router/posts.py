b00178efbe7c1d1fddc6a2294153c500a15d82bc
Lavi Ionas <laviionas772@gmail.com>
2025-03-15 12:13:40 -0400

    created post test_posts_sent

--- router/posts.py
+++ router/posts.py
@@ -17,5 +17,14 @@ def test_posts(db: Session = Depends(get_db)):
 
     post = db.query(models.Post).all()
 
-
     return  post
+
+@router.post('/', status_code=status.HTTP_201_CREATED, response_model=List[schemas.CreatePost])
+def test_posts_sent(post_post:schemas.CreatePost, db:Session = Depends(get_db)):
+
+    new_post = models.Post(**post_post.dict())
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
+
+    return [new_post]
