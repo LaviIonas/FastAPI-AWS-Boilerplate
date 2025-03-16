5108f5f5e4dfc2991b78980edb03f2a7aeccf064
Lavi Ionas <laviionas772@gmail.com>
2025-03-16 14:05:36 -0400

    middleware and new routers

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from database import engine
 from models import Base
@@ -6,20 +7,29 @@ from models import Base
 from router.posts import router as posts_router
 from router.papers import router as papers_router
 from router.user import router as user_router
+from router.auth import router as auth_router
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(posts_router)
 app.include_router(papers_router)
 app.include_router(user_router)
+app.include_router(auth_router)
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def root():
+    return {"message": "Welcome to the Research Paper Management API"}
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
 
